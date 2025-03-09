c09788f3eee2467623f9095be8a8aa62c52d5aa2
BHNGDY <JE2J91@163.com>
2025-03-09 13:40:05 +0800

    PRE-CLASSIC

---
+++ 刀削面传奇preclassic.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+print('刀削面传奇')
+print('Pre-Classic')
+print('This is a little test of the "ramdom!"')
+x = 1
+for x in range (2):
+  print('刀削面')
+  print('哦刀削面')
+input('ENTER开始新的一天')
+pc = ("刀削面","卤蛋","面","香菜",)
+fs = 0
+sc = 0
+th = 0
+fo = 0
+print("您有",pc[0],fs,"碗")
+print("您有",pc[1],sc,"个")
+print("您有",pc[2],th,"份")
+print("您有",pc[3],fo,"份")
+st = random.randint(3,9)
+time.sleep(st)
+print('一位顾客来到！')
+print('他/她要')
+
+
